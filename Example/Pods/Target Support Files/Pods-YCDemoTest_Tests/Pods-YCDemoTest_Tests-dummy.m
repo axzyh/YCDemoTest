62ec1e12fc6b99e17512ad0b9f85db0594a9f7cf
hongyuchao <5327772@qq.com>
2023-12-21 17:53:24 +0800

    提交

---
+++ Example/Pods/Target Support Files/Pods-YCDemoTest_Tests/Pods-YCDemoTest_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YCDemoTest_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_YCDemoTest_Tests
+@end
---
+++ Example/Pods/Target Support Files/YCDemoTest/YCDemoTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_YCDemoTest : NSObject
+@end
+@implementation PodsDummy_YCDemoTest
+@end
